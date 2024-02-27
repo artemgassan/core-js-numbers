79336fa059aa64dc53ab5eb7f350a6df28260c2e
artemgassan <artemgassan@ya.ru>
2024-02-27 17:01:35 +0300

    feat: finsih toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -228,8 +228,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const numberValue = Number(value);
+  return Number.isNaN(numberValue) ? def : numberValue;
 }
 
 /**
